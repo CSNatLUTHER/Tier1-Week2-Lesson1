c26ce71d73ad3fce28fc52b6127fee0650796665
Chris Nelson <cnelson04@gmail.com>
2021-08-13 20:29:21 -0500

    stretch goals completed

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -29,17 +29,70 @@ console.log('5. Adding 25 to supplyChanges.', supplyChanges);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
+for( let i=0; i < supplyChanges.length; i++) {
+    // console.log(supplyChanges[i]);
+    if (supplyChanges[i] > 0){
+        console.log ( 'Added ', supplyChanges[i], ' parts.')
+    }
+    else if (supplyChanges[i] === 0){
+        console.log ( 'No change.')
+    }
+    else{
+        console.log ('Removed ', supplyChanges[i], ' parts.')
+    }
+}
+
 
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
+
 console.log('7. Showing supplyChanges with "for of" loop');
+for (let value of supplyChanges){
+    if (value > 0){
+        console.log ( 'Added ', value, ' parts.')
+    }
+    else if (value === 0){
+        console.log ( 'No change.')
+    }
+    else{
+        console.log ('Removed ', value, ' parts.')
+    }
+}
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
+let reviewedSupplyChanges = [];
+confirm( 'Do you want to check Supply Changes?')
+while( supplyChanges.length > 0 ){
+    if (supplyChanges[0] > 0){
+        console.log ( 'Added ', supplyChanges[0], ' parts.');
+        reviewedSupplyChanges.push(supplyChanges.shift(0))
+    }
+
+    else if (supplyChanges[0] === 0){
+        console.log ( 'No change.');
+        reviewedSupplyChanges.push(supplyChanges.shift(0))
+    }
+    else{
+        console.log ('Removed ', supplyChanges[0], ' parts.');
+        reviewedSupplyChanges.push(supplyChanges.shift(0))
+    }
+}
+    //cofirm reviewed changes:
+    console.log ( 'Reviewed Changes: ', reviewedSupplyChanges)
+    alert ( 'The following changes were reviewed: ' + reviewedSupplyChanges)
+    // add supply changes back into supplyChanges array
+    supplyChanges = reviewedSupplyChanges
+    console.log ('Supply Changes: ', supplyChanges)
 
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
-console.log('9. Total supplies available is:');
+
+let totalSupply = 0;
+for( let i=0; i < supplyChanges.length; i++) {
+    totalSupply += supplyChanges[i];
+}
+console.log('9. Total supplies available is: ', totalSupply);
